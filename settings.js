daa6c3fdb235061dccb1f67b59511f0cc2ac1432
hoseinbaloch1 <110195595+hoseinbaloch1@users.noreply.github.com>
2023-01-28 23:29:18 +0330

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
+global.owner = ['989339658384'] //ur owner number
+global.ownername = "Hossein Sarbazi" //ur owner name
+global.ytname = "YT: BALOCH EDIT" //ur yt chanel name
 global.socialm = "GitHub: nimaofficial" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
+global.ownernomer = "989339658384" //ur number
+global.premium = ['989339658384'] //ur premium number
+global.botname = 'HOSEIN BOT' //ur bot name
+global.linkz = "https://chat.whatsapp.com/DtpQbVzujrfLRB2xvl97pp" //your theme url which will be displayed on whatsapp
 global.websitex = "https://www.helacloud.ga" //ur website to be displayed
 global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
 global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
+global.packname = "Sticker baloch edit by hosein" //ur sticker watermark packname
 global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.wm = "hoseinok" //ur watermark
 
 // Other
 global.sessionName = 'session'
